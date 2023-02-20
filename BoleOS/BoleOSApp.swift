2ff14607f9e821f1c7f7d43668eca2e7f0f09dfd
Josh Boles <josh@joshboles.com>
2023-02-20 17:24:09 -0500

    Initial Commit

---
+++ BoleOS/BoleOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoleOSApp.swift
+//  BoleOS
+//
+//  Created by Josh Boles on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct BoleOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
